4f27590550f5b0f644e746c180a5a5407102c449
damian szalinski <dszalinski979@gmail.com>
2019-07-01 22:41:01 +0200

    New version release

---
+++ WordUnscrambler/Workers/WordMatcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordUnscrambler
+{
+    class WordMatcher
+    {
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        {
+            var matchedWords = new List<MatchedWord>();
+
+            foreach (var scrambleWord in scrambledWords)
+            {
+                
+                foreach (var word in wordList)
+                {
+                    if (scrambleWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scrambleWord, word));
+                    }
+                    else
+                    {
+                        var scrambledWordArray = scrambleWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambledWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrabledWord = new string(scrambledWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if (sortedScrabledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scrambleWord, word));
+                        }
+                    }
+                }
+            }
+
+            return matchedWords;
+        }
+
+        private MatchedWord BuildMatchedWord(string scrambledWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWord = scrambledWord,
+                Word = word
+            };
+
+            return matchedWord;
+        }
+    }
+}
